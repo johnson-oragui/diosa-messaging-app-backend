64862a0dd104d78178abc418c0ca96de3dbb0a25
johnson-oragui <johnson-oragui@gmail.com>
2024-10-06 12:15:12 +0100

    feat: added social_login tables, modified set_password method, added users.profile relationship

--- backend/app/v1/users/models.py
+++ backend/app/v1/users/models.py
@@ -1,6 +1,7 @@
 from typing import List
 from passlib.context import CryptContext
 from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 from app.database.session import (
     Base,
@@ -10,6 +11,7 @@ from app.database.session import (
     mapped_column
 )
 from app.v1.chats import Message
+from app.v1.profile import Profile
 
 password_context = CryptContext(
     schemes=["bcrypt"],
@@ -24,9 +26,11 @@ class User(ModelMixin, Base):
     first_name: Mapped[str] = mapped_column(String(30), nullable=True)
     last_name: Mapped[str] = mapped_column(String(30), nullable=True)
     email: Mapped[str] = mapped_column(String(30), unique=True)
-    password: Mapped[str] = mapped_column(String(100))
+    password: Mapped[str] = mapped_column(String(100), nullable=True)
     username: Mapped[str] = mapped_column(String(30), unique=True)
     idempotency_key: Mapped[str] = mapped_column(String(120), unique=True)
+    email_verified: Mapped[bool] = mapped_column(default=False)
+    status: Mapped[str] = mapped_column(server_default="active")
 
     messages: Mapped[List["Message"]] = relationship(
         "Message", back_populates="user", cascade="all, delete-orphan"
@@ -39,19 +43,28 @@ class User(ModelMixin, Base):
     rooms_belongs_to = relationship(
        "Room_Member", back_populates="member", cascade="all, delete-orphan"
     )
+    profile: Mapped["Profile"] = relationship(
+       "Profile", back_populates="user", cascade="all, delete-orphan", uselist=False
+    )
 
     def set_password(self, plain_password: str) -> None:
         """
         Sets a user password.
+
+        Args:
+            plain_password(str): password to hash.
         """
-        if not isinstance(plain_password, str) or not plain_password:
-            raise Exception(f'{plain_password} must be a string')
+        if not plain_password or plain_password == "":
+            return
         hashed_password = password_context.hash(plain_password)
         self.password = hashed_password
 
     def verify_password(self, plain_password) -> bool:
         """
         Verifies user password.
+
+        Args:
+            plain_password(str): password to compare with hash.
         """
         if not plain_password:
             raise Exception(f'{plain_password} must be provided')
@@ -59,3 +72,18 @@ class User(ModelMixin, Base):
             secret=plain_password,
             hash=self.password
         )
+
+class Social_Login(ModelMixin, Base):
+    """
+    Social Login Model(Table)
+    """
+    user_id: Mapped[str] = mapped_column(
+        String(60), ForeignKey("users.id", ondelete="CASCADE")
+    )
+    # Social login provider (e.g., "google", "facebook", "github")
+    provider: Mapped[str] = mapped_column()
+    # Unique identifier for the user on the social login provider
+    sub: Mapped[str] = mapped_column(String(60))
+    access_token: Mapped[str] = mapped_column()
+    id_token: Mapped[str] = mapped_column(nullable=True)
+    refresh_token: Mapped[str] = mapped_column(nullable=True)
