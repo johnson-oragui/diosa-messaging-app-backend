a7017768959b5a1bf812c9c3f65d5fefc6febfa0
johnson-oragui <johnson-oragui@gmail.com>
2024-10-06 12:08:00 +0100

    feat: added authentication functions dependencies

--- backend/app/v1/auth/dependencies.py
+++ backend/app/v1/auth/dependencies.py
@@ -1,12 +1,28 @@
-from fastapi import status, HTTPException, Request
+from typing import Optional, Tuple
+from fastapi import (
+    status,
+    HTTPException,
+    Request,
+    Cookie,
+    Header,
+)
 from datetime import datetime, timezone, timedelta
 from jose import jwt, JWTError
 import hashlib
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2,
+)
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.config import settings
 from app.utils.task_logger import create_logger
+from app.v1.users.services import user_service
+from app.v1.users import User
 
 logger = create_logger("Auth Dpenedencies")
+
+oauth2_scheme: OAuth2 = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/token")
 
 async def generate_idempotency_key(email: str, username: str) -> str:
     """Creates an idempotency key
@@ -16,6 +32,8 @@ async def generate_idempotency_key(email: str, username: str) -> str:
         username(str) -- username
     Return: Idempotency_key(str)
     """
+    if not email or not username:
+        raise Exception("email or username cannot be None")
     key = f"{email}:{username}"
     idempotency_key: str = hashlib.sha256(key.encode()).hexdigest()
     return idempotency_key
@@ -32,7 +50,7 @@ async def generate_jwt_token(data: dict, token_type: str = "access") -> str:
     
     now = datetime.now(timezone.utc)
     if token_type == "refresh":
-        expire = now + timedelta(minutes=settings.jwt_access_token_expire_minutes)
+        expire = now + timedelta(days=settings.jwt_refresh_token_expire_days)
     elif token_type == "access":
         expire = now + timedelta(minutes=settings.jwt_access_token_expire_minutes)
     else:
@@ -50,8 +68,32 @@ async def generate_jwt_token(data: dict, token_type: str = "access") -> str:
         key=settings.secrets,
         algorithm=settings.jwt_algorithm
     )
+    logger.info(msg=f"token: {token}")
     return token
 
+async def generate_access_and_refresh(user_id: str, request: Request) -> Tuple[str, str]:
+        """
+        Generates both access and refresh tokens.
+
+        Args:
+            user_id(str): id of the user.
+            request: request object
+        Returns:
+            tuple(str, str): generated access and refresh tokens
+        """
+        access_token = await generate_jwt_token({
+            "user_id": user_id,
+            "user_agent": request.headers.get("user-agent"),
+            "user_ip": request.client.host
+        })
+
+        refresh_token = await generate_jwt_token({
+            "user_id": user_id,
+            "user_agent": request.headers.get("user-agent"),
+            "user_ip": request.client.host
+        }, token_type="refresh")
+
+        return access_token, refresh_token
 
 async def verify_access_token(token: str, request: Request) -> str:
     """Verifies/Decodes access token.
@@ -61,7 +103,6 @@ async def verify_access_token(token: str, request: Request) -> str:
         request(Request) -- request object
     Return: user_id
     """
-    
     try:
         claims: dict = jwt.decode(
             token,
@@ -77,6 +118,7 @@ async def verify_access_token(token: str, request: Request) -> str:
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Cannot Use Refresh Token"
             )
+
         if user_agent != claims.get("user_agent") or user_ip != claims.get("user_ip"):
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
         return user_id
@@ -122,3 +164,177 @@ async def verify_refresh_token(token: str, request: Request) -> str:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED
         )
+
+async def check_for_access_token(access_token: Optional[str] = Cookie(None),
+                                 Authorization: Optional[str] = Header(None)) -> Optional[str]:
+    """
+    Checks for token in cookies, if not present, checks for token in header.
+
+    Args:
+        access_token(str): token from cookie.
+        Authorization(str): token from header.
+    Returns:
+        token(str): if found, None if not found.
+    """
+    token = None
+    if access_token:
+        token = access_token
+        logger.info(msg=f"access_token: {access_token}")
+    elif Authorization:
+        logger.info(msg=f"Authorization: {Authorization}")
+        try:
+            token_type, token_value = Authorization.split(" ")
+            if token_type.lower() != "bearer":
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Invalid Token Type"
+                )
+            if not token_value or token_value.strip() == "":
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Token Cannot be Null"
+                )
+            token = token_value
+        except ValueError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid Authentication Header"
+            )
+    else:
+        # If no token is found in both places, raise an exception
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token not provided"
+        )
+    return token
+
+async def check_refresh_token(refresh_token: Optional[str] = Cookie(None),
+                              Authorization: Optional[str] = Header(None)) -> Optional[str]:
+    """
+    Checks for refresh token in the cookies.
+
+    Args:
+        refresh_token(str): refresh_token from cookie.
+        Authorization(str): refresh_token from header.
+    Returns:
+        token(str): if found, None if not found.
+    """
+    token = None
+    if refresh_token:
+        token = refresh_token
+    elif Authorization:
+        try:
+            token_type, token_value = Authorization.split(" ")
+            if token_type.lower() != "bearer":
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Invalid Refresh-Token Type"
+                )
+            if not token_value or token_value.strip() == "":
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Refresh-Token Cannot be Null"
+                )
+            token = token_value
+        except ValueError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid Authentication Header"
+            )
+    else:
+        # If no token is found in both places, raise an exception
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Refresh-Token not provided"
+        )
+    return token
+
+async def authenticate_user(schema: dict, session: AsyncSession) -> Optional[User]:
+        """
+        Authenticates a user's email/username and password.
+
+        Args:
+            schema(dict): username/email and password.
+            session: Database session object.
+        Returns:
+            user(object): The user if authentication is successful
+        Raise:
+            HTTPException: if authentication is unsuccessful.
+        """
+        user = await user_service.fetch_by_email_or_user_name({
+            "email": schema.get("username", ""),
+            "username": schema.get("username", "")
+        }, session)
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Invalid username/email or password"
+            )
+
+        if user.status != "active":
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="User is inactive"
+            )
+        
+        is_valid_password = user.verify_password(schema.get("password"))
+        if not is_valid_password:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Invalid username/email or password"
+            )
+        return user
+
+async def get_current_user(
+        access_token: str,
+        request:Request,
+        session: AsyncSession
+    ) -> Optional[User]:
+        """
+        Retrieves the current user using the access_token.
+
+        Args:
+            access_token(str): access_token of the user.
+            request(Object): request object.
+            session(object): database session object.
+        Returns:
+            USER(object): if access_token is valid.
+        Raises:
+            HTTPException: If user not found.
+        """
+        user_id: str = await verify_access_token(access_token, request)
+
+        user = await user_service.fetch({"id": user_id}, session)
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not found."
+            )
+        return user
+
+async def get_current_active_user(
+        access_token: str,
+        request:Request,
+        session: AsyncSession
+    ) -> Optional[User]:
+        """Retrieves the current-active user using the access_token.
+
+        Args:
+            access_token(str): access_token of the user.
+            request(Object): request object.
+            session(object): database session object.
+        Returns:
+            USER(object): if access_token is valid.
+        Raises:
+            HTTPException: If user is not active.
+
+        """
+        user: User | None = await get_current_user(access_token, request, session)
+        if user.status != "active":
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="User is inactive"
+            )
+        return user
