a5d84f9aa11e46cd4c61d0f3a93bdd90f1d4aaed
johnson-oragui <johnson-oragui@gmail.com>
2024-10-07 10:44:11 +0100

    feat: added main App component

---
+++ frontend/chatroom/src/App.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+
+import './App.css';
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <div id="detail">
+        <Outlet />
+      </div> 
+    <Footer /> 
+    </>
+  )
+}
+
+export default App
