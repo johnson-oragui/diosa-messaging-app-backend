d210062f5bc405e9c0518d7c1deff913ab1dd89b
johnson-oragui <johnson-oragui@gmail.com>
2024-10-07 10:41:10 +0100

    feat: added Login component

---
+++ frontend/chatroom/src/components/Login.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import LoginForm from "@/components/LoginForm";
+
+export default function Login() {
+
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <>
+        <div>
+            <h1 className="">
+                Login
+            </h1>
+            <LoginForm />
+        </div>
+        </>
+    );
+};
