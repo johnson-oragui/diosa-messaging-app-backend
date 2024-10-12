385380818ad72bc26efc615ef401509a7be7ed80
johnson-oragui <johnson-oragui@gmail.com>
2024-10-12 12:24:23 +0100

    feat: added roominvitations table, rooms.room_type, rooms.message_deletable, roommembers.room_type

--- backend/app/v1/rooms/models.py
+++ backend/app/v1/rooms/models.py
@@ -1,3 +1,4 @@
+from typing import TYPE_CHECKING
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey, UniqueConstraint
 
@@ -11,32 +12,46 @@ from app.database.session import (
     datetime,
     DateTime
 )
-# from app.v1.chats import Message
+from app.base.enums import (
+    room_type_enum,
+    invitation_status_enum
+)
+
+if TYPE_CHECKING:   
+    from app.v1.chats import Message
+    from app.v1.users import User
 
 
 class Room(ModelMixin, Base):
     """
     Class representing rooms table
     """
-    name: Mapped[str] = mapped_column(String(40))
-    owner_id: Mapped[str] = mapped_column(
+    room_name: Mapped[str] = mapped_column(String(40))
+    creator_id: Mapped[str] = mapped_column(
         String(60), ForeignKey("users.id", ondelete="CASCADE"), index=True
     )
-    is_private: Mapped[bool] = mapped_column(default=True)
+    room_type: Mapped[str] = mapped_column(room_type_enum, server_default="private")
 
-    owner = relationship(
+    description: Mapped[str] = mapped_column(nullable=True)
+    messages_deletable: Mapped[bool] = mapped_column(default=True)
+
+    owner: Mapped["User"] = relationship(
         "User", back_populates="rooms_created", uselist=False
     )
-    messages = relationship(
+    messages: Mapped["Message"] = relationship(
         "Message", back_populates="room", cascade="all, delete-orphan"
     )
 
-    room_members = relationship(
-        "Room_Member", back_populates="room", cascade="all, delete-orphan"
+    room_members: Mapped["RoomMember"] = relationship(
+        "RoomMember", back_populates="room", cascade="all, delete-orphan"
+    )
+
+    invitations: Mapped["RoomInvitation"] = relationship(
+        "RoomInvitation", back_populates="room", cascade="all, delete-orphan"
     )
 
 
-class Room_Member(ModelMixin, Base):
+class RoomMember(ModelMixin, Base):
     """
     Class representing room_members
     """
@@ -49,15 +64,49 @@ class Room_Member(ModelMixin, Base):
     invited_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
+    room_type: Mapped[str] = mapped_column(room_type_enum)
     is_admin: Mapped[bool] = mapped_column(default=False)
 
-    member = relationship(
+    member: Mapped["User"] = relationship(
         "User", back_populates="rooms_belongs_to", uselist=False
     )
-    room = relationship(
+    room: Mapped["Room"] = relationship(
         "Room", back_populates="room_members", uselist=False
     )
 
     __table_args__ = (
         UniqueConstraint(room_id, user_id, name="uq_room_member_room_id_user_id"),
+    )
+
+
+class RoomInvitation(ModelMixin, Base):
+    """
+    Class representing invitations to join private rooms.
+    """
+    room_id: Mapped[str] = mapped_column(
+        String(60), ForeignKey("rooms.id", ondelete="CASCADE"), index=True
+    )
+    inviter_id: Mapped[str] = mapped_column(
+        String(60), ForeignKey("users.id", ondelete="CASCADE"), index=True
+    )
+    invitee_id: Mapped[str] = mapped_column(
+        String(60), ForeignKey("users.id", ondelete="CASCADE"), index=True
+    )
+    invitation_status: Mapped[str] = mapped_column(invitation_status_enum, server_default="pending")
+    room_type: Mapped[str] = mapped_column(room_type_enum)
+
+    room: Mapped["Room"] = relationship(
+        "Room", back_populates="invitations", uselist=False
+    )
+    inviter: Mapped["User"] = relationship(
+        "User", foreign_keys=[inviter_id], back_populates="sent_invitations", uselist=False
+    )
+    invitee: Mapped["User"] = relationship(
+        "User", foreign_keys=[invitee_id], back_populates="received_invitations", uselist=False
+    )
+    
+    __table_args__ = (
+        UniqueConstraint(
+            room_id, invitee_id, name="uq_room_invitation_room_id_invitee_id"
+        ),
     )
