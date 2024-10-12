db1fef0df8433384d29415bef36555ae531a100a
johnson-oragui <johnson-oragui@gmail.com>
2024-10-12 12:22:47 +0100

    feat: added chat-type to messages table

--- backend/app/v1/chats/models.py
+++ backend/app/v1/chats/models.py
@@ -1,5 +1,5 @@
-from typing import List
-from sqlalchemy import Integer, ForeignKey
+from typing import TYPE_CHECKING
+from sqlalchemy import Integer, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 
 from app.database.session import (
@@ -11,10 +11,13 @@ from app.database.session import (
 )
 from app.v1.rooms import Room
 
+if TYPE_CHECKING:
+    from app.v1.users import User
+
 
 class Message(ModelMixin, Base):
     """
-    Class Message mapping users table in the database
+    Class Message mapping users table in the database.
     """
     id: Mapped[int] = mapped_column(
         Integer, primary_key=True, index=True, unique=True, autoincrement=True
@@ -26,9 +29,11 @@ class Message(ModelMixin, Base):
         String(60), ForeignKey("rooms.id", ondelete="CASCADE"), index=True
     )
     content: Mapped[str] = mapped_column(String(1000))
-    chat_type: Mapped[str] = mapped_column(String(30), nullable=True)
+    chat_type: Mapped[str] = mapped_column(
+        Enum("public", "private", "direct_message", name="chat_type_enum", validate_strings=True, schema="chat")
+    )
 
-    user = relationship(
+    user: Mapped["User"] = relationship(
         "User", back_populates="messages", uselist=False
     )
     room: Mapped["Room"] = relationship(
