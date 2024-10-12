8b4c8696bbd441eae2f4ed2c1e91a9aa2e0efdb7
johnson-oragui <johnson-oragui@gmail.com>
2024-10-12 12:33:53 +0100

    feat: added custom exceptions

---
+++ backend/app/core/custom_exceptions.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+
+class RoomNotFoundError(Exception):
+    """
+    Custom exception class for Room-not-found.
+    """
+    def __init__(self, message: str, *args: Any, **kwargs: Any) -> None:
+        super().__init__(message)
+
+class UserNotAMemberError(RoomNotFoundError):
+    """
+    Custom error for user not a room member
+    """
+    pass
+
+class UserNotAnAdminError(RoomNotFoundError):
+    """
+    Custom error for user not an admin.
+    """
+    pass
+
+class InvitationNotFoundError(RoomNotFoundError):
+    """
+    Custom error for invitation not found.
+    """
+    pass
+
+class CannotDeleteMessageError(RoomNotFoundError):
+    """
+    Custom error for messages that cannot be deleted.
+    """
+    pass
