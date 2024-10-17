959660b56f4130b6cbdf033251a77d8eba7373df
johnson-oragui <johnson-oragui@gmail.com>
2024-10-17 14:31:17 +0100

    feat: added the APIROuters to the main router

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,10 @@ api_version_one = APIRouter(prefix="/api/v1")
 
 from app.v1.auth.routes import auth
 from app.v1.users.routes import *
+from app.v1.chats.routes import chats
+from app.v1.rooms.routes import *
 
 api_version_one.include_router(router=auth)
 api_version_one.include_router(router=users)
+api_version_one.include_router(router=chats)
+api_version_one.include_router(router=rooms)
