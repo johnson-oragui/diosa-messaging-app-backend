4f52db245b54c0ce36328f0e21607fbfe8bf3197
johnson-oragui <johnson-oragui@gmail.com>
2024-10-24 13:32:37 +0100

    feat: added schema for message update

--- backend/app/v1/chats/schemas.py
+++ backend/app/v1/chats/schemas.py
@@ -1,17 +1,21 @@
-from typing import List, Optional
+from typing import List, Optional, Annotated
 from datetime import datetime, timezone
 from pydantic import (
     BaseModel,
     Field,
-    HttpUrl
+    HttpUrl,
+    StringConstraints,
+    model_validator
 )
+from bleach import clean
+from unicodedata import normalize
 
 class BaseMessage(BaseModel):
     """
     Schema for individual message.
     """
     room_id: str = Field(
-        examples=["Messages Retrieved Successfully"]
+        examples=["213e3d.415f5.h7j64..."]
     )
     content: str = Field(
         examples=["Good day to you."]
@@ -50,3 +54,44 @@ class MessageDeleteSchema(BaseModel):
     message_ids: List[int] = Field(
         examples=[1]
     )
+
+class UpdateMessageInput(BaseModel):
+    """
+    Class for updating a message.
+    """
+    message: Annotated[
+        str,
+        StringConstraints(
+            strip_whitespace=True,
+            min_length=1
+        )
+    ]
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_message(cls, values: dict):
+        """
+        Validates message.
+        """
+        message: str = values.get("message", "")
+
+        if not message:
+            raise ValueError("message cannot be empty")
+        if len(message) < 2 and message.startswith(" "):
+            raise ValueError("message cannot contain only a whitespace")
+        if message.strip(" ") == "":
+            raise ValueError("message cannot be empty")
+
+        values["message"] = normalize("NFKC", clean(message))
+
+        return values
+
+class UpdateMessageResponse(AllMessagesResponse):
+    """
+    Class for message update response.
+    """
+    message: str = Field(
+        default="Message updated successfully.",
+        examples=["Message updated successfully."]
+    )
+    data: BaseMessage
