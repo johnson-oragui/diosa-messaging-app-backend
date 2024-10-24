0c9e581f5fa72f9d155115c1e67012c3e860080c
johnson-oragui <johnson-oragui@gmail.com>
2024-10-24 09:38:46 +0100

    tests: added test for rooms-fields update route

--- backend/app/v1/tests/room/test_room_routes.py
+++ backend/app/v1/tests/room/test_room_routes.py
@@ -2,6 +2,7 @@ import pytest
 
 from app.v1.users.services import user_service
 from app.v1.rooms.services import room_service
+from app.v1.rooms.services import room_member_service
 from app.v1.profile.services import profile_service
 from app.v1.chats.services import message_service
 
@@ -1336,3 +1337,75 @@ class TestCreateDirectMessageRoomRoute:
             'message': 'User is not allowed to delete the message',
             'data': {}
         }
+
+    @pytest.mark.asyncio
+    async def test_update_room_fields_route(self,
+                                               client,
+                                               mock_johnson_user_dict,
+                                               mock_jayson_user_dict,
+                                               mock_user_id,
+                                               test_get_session,
+                                               test_setup):
+        """
+        Tests successfull updating of roo-field route.
+        """
+        # create jayson user
+        jayson = await user_service.create(
+            mock_jayson_user_dict,
+            test_get_session
+        )
+        _ = await profile_service.create(
+            {"user_id": jayson.id},
+            test_get_session
+        )
+
+        mock_johnson_user_dict["id"] = mock_user_id
+
+        # create johnson user
+        johnson = await user_service.create(
+            mock_johnson_user_dict,
+            test_get_session
+        )
+        _ = await profile_service.create(
+            {"user_id": mock_user_id},
+            test_get_session
+        )
+
+        # jayson creates room, and messages are not deletable
+        new_room, _, _ =await room_service.create_a_public_or_private_room(
+            room_name="public-room-name",
+            creator_id=johnson.id,
+            session=test_get_session,
+            messages_deletable=False,
+            room_type="public"
+        )
+
+        await room_member_service.create(
+            {
+                "room_id": new_room.id,
+                "user_id": jayson.id,
+                "room_type": "public",
+            },
+            test_get_session
+        )
+
+        print("new_room: ", new_room.id)
+        # request to update room.messages_deletable and room_type
+        response = client.put(
+            url=f"/api/v1/rooms/{new_room.id}",
+            json={
+                "messages_deletable": True,
+                "room_type": "private",
+                "description": "great one",
+            },
+            headers={
+                "Authorization": "Bearer fake",
+            }
+        )
+
+        assert response.status_code == 201
+        data = response.json()
+
+        assert data["data"]["room_type"] == "private"
+        assert data["data"]["messages_deletable"] == True
+        assert data["data"]["description"] == "great one"
