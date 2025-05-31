f1babf09034bde498c3b2382afaa7fdb7fd3f2a1
johnson-oragui <johnson.oragui@gmail.com>
2025-05-31 13:01:36 +0100

    feat: added obfuscation to validation_exception_handler

--- backend/app/core/error_handlers.py
+++ backend/app/core/error_handlers.py
@@ -1,111 +1,164 @@
 """
 Exception handler module
 """
+
 from fastapi import HTTPException, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.exc import SQLAlchemyError
 from redis.exceptions import RedisError
+from slowapi.errors import RateLimitExceeded
 
 from app.utils.task_logger import create_logger
 
 logger = create_logger("Exception Handler Logger")
 
+
 def get_user_ip_and_agent(request: Request) -> dict:
     """
     Retrieves user_ip and user_agent
     """
-    user_ip = request.client.host
-    user_agent = request.headers.get('user-agent', 'Unknown')
+    user_ip = request.client and request.client.host or None
+    user_agent = request.headers.get("user-agent", "Unknown")
     path = request.url.path
     method = request.method
-    return {"user_ip": user_ip, "user_agent": user_agent, "path": path, "method": method}
+    current_user = (
+        request.state.current_user
+        if hasattr(request.state, "current_user")
+        else "Guest"
+    )
+    return {
+        "user_ip": user_ip,
+        "user_agent": user_agent,
+        "path": path,
+        "method": method,
+        "current_user": current_user,
+    }
+
 
 async def exception(request: Request, exc: Exception) -> JSONResponse:
     """
     Returns 500 status code
     """
     logger.error(
-        msg=f"Unhandled Exception: {exc}",
-        extra=get_user_ip_and_agent(request)
+        msg=f"Unhandled Exception: {exc}", extra=get_user_ip_and_agent(request)
     )
     return JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content=jsonable_encoder({
-            "status_code": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            "message": "An Unexpected Error Occured" ,
-            "data": {}
-        })
+        content=jsonable_encoder(
+            {
+                "status_code": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "message": "An Unexpected Error Occured",
+                "data": {},
+            }
+        ),
     )
+
 
 async def http_exception(request: Request, exc: HTTPException) -> JSONResponse:
     """
     Handles Http Exceptions
     """
     logger.error(
-        msg=f"Http Exception: {exc.detail}",
-        extra=get_user_ip_and_agent(request)
+        msg=f"Http Exception: {exc.detail}", extra=get_user_ip_and_agent(request)
     )
 
     return JSONResponse(
         status_code=exc.status_code,
-        content=jsonable_encoder({
-            "status_code": exc.status_code,
-            "message": exc.detail,
-            "data": {}
-        })
+        content=jsonable_encoder(
+            {"status_code": exc.status_code, "message": exc.detail, "data": {}}
+        ),
     )
 
-async def validation_excption_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+
+async def validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> JSONResponse:
     """
     Handles request validation error
     """
-    error = exc.errors() if exc.errors() else "No Error message to extract from"
-    logger.error(
-        msg=f"Validation error: {error}",
-        extra=get_user_ip_and_agent(request)
-    )
+    error = exc.errors()[0] if exc.errors() else "No Error message to extract from"
+
+    sensitive_words = ["password", "confirm_password", "secret_token"]
+
+    if isinstance(error, dict) and len(error) > 0:
+
+        for key in error.keys():
+            if key == "input":
+                input = error.get("input")
+                if isinstance(input, dict):
+                    for k in input.keys():
+                        if k in sensitive_words:
+                            error["input"][k] = "************"
+                        if k == "device_info" and "device_id" in error["input"][k]:
+                            error["input"][k]["device_id"] = "*************"
+
+    logger.error(msg=f"Validation error: {error}", extra=get_user_ip_and_agent(request))
 
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({
-            "status_code": status.HTTP_422_UNPROCESSABLE_ENTITY,
-            "message": "Validation Error.",
-            "data": exc.errors()[0] if exc.errors()[0] else "Empty Error"
-        })
+        content=jsonable_encoder(
+            {
+                "status_code": status.HTTP_422_UNPROCESSABLE_ENTITY,
+                "message": "Validation Error.",
+                "data": error,
+            }
+        ),
     )
 
-async def sqlalchemy_exception_handler(request: Request, exc: SQLAlchemyError) -> JSONResponse:
+
+async def sqlalchemy_exception_handler(
+    request: Request, exc: SQLAlchemyError
+) -> JSONResponse:
     """
     Handles sqlalchemy error
     """
-    logger.error(
-        msg=f"Sqlachemy Error: {exc}",
-        extra=get_user_ip_and_agent(request)
-    )
+    logger.error(msg=f"Sqlachemy Error: {exc}", extra=get_user_ip_and_agent(request))
+
     return JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content=jsonable_encoder({
-            "status_code": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            "message": "Internal Server Error",
-            "data": {}
-        })
+        content=jsonable_encoder(
+            {
+                "status_code": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "message": "Internal Server Error",
+                "data": {},
+            }
+        ),
     )
+
 
 async def redis_exception_handler(request: Request, exc: RedisError) -> JSONResponse:
     """
     Handles Redis Error
     """
-    logger.error(
-        msg=f'Redis error: {exc}',
-        extra=get_user_ip_and_agent(request)
-    )
+    logger.error(msg=f"Redis error: {exc}", extra=get_user_ip_and_agent(request))
     return JSONResponse(
         status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-        content=jsonable_encoder({
-            "status_code": status.HTTP_503_SERVICE_UNAVAILABLE,
-            "message": "A Redis Error occured",
-            "data": {}
-        })
+        content=jsonable_encoder(
+            {
+                "status_code": status.HTTP_503_SERVICE_UNAVAILABLE,
+                "message": "A Redis Error occured",
+                "data": {},
+            }
+        ),
+    )
+
+
+async def ratelimit_exception_handler(
+    request: Request, exc: RateLimitExceeded
+) -> JSONResponse:
+    """
+    Handles ratelimit Error
+    """
+    logger.error(msg=f"RateLimit error: {exc}", extra=get_user_ip_and_agent(request))
+    return JSONResponse(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        content=jsonable_encoder(
+            {
+                "status_code": status.HTTP_429_TOO_MANY_REQUESTS,
+                "message": "Too many login attempts. Please try again later.",
+                "data": {},
+            }
+        ),
     )
