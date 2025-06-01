a28bb0d40b6cd4392d7e4704010cbd1668156f6d
johnson-oragui <johnson.oragui@gmail.com>
2025-06-01 20:21:32 +0100

    feat: added test fixtures

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,49 +1,70 @@
 from typing import AsyncGenerator
-from httpx import AsyncClient
 import pytest
-from fastapi import FastAPI
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+)
+from sqlalchemy import StaticPool
+
 
 from main import app
-from app.utils.celery_setup.setup import make_celery
 from app.core.config import settings
-from app.database.celery_database import setup_celery_results_db
-from app.database.session import get_async_session
+from app.database.session import get_async_session, Base
 
 
-@pytest.fixture(scope="session")
-def main_app() -> FastAPI:
-    """
-    Returns FastAPI app instance for testing
-    """
-    return app
+# SQLite-specific configuration for local testing
+test_engine = create_async_engine(
+    url=settings.db_url_test,
+    connect_args={
+        "check_same_thread": False,
+    },
+    poolclass=StaticPool,
+)
+
+
+TestSessionLocalMemory = async_sessionmaker(
+    bind=test_engine, autoflush=False, autocommit=False, expire_on_commit=False
+)
 
 
 @pytest.fixture(scope="function")
-async def client(main_app: FastAPI) -> AsyncGenerator[AsyncClient, None]:
+async def test_get_session():
+    """
+    Replaces get_session function for method testing.
+    """
+    async with TestSessionLocalMemory() as session:
+        yield session
+        await session.close()
+
+
+async def override_get_async_session():
+    """
+    Overrides get_async_session generator in app instance.
+    """
+    async with TestSessionLocalMemory() as session:
+        yield session
+        await session.close()
+
+
+app.dependency_overrides[get_async_session] = override_get_async_session
+
+
+@pytest.fixture(scope="function")
+async def client() -> AsyncGenerator[AsyncClient, None]:
     """
     HTTPX async test client fixture
     """
-    async with AsyncClient(app=main_app, base_url="http://test") as client_:
-        yield client_
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
 
 
+# create database session for testing service classes
 @pytest.fixture(scope="function")
-async def test_session():
+async def test_setup():
     """
-    Removes test users
+    create database session for testing service classes
     """
-    async for session in get_async_session():
-        yield session
-
-
-@pytest.fixture(scope="session")
-def celery_app():
-    """
-    Configures celery
-    """
-    setup_celery_results_db()
-    celery_app = make_celery(
-        broker_url=settings.celery_broker_url_test,
-        result_backend=settings.celery_result_backend_test,
-    )
-    return celery_app
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        yield
