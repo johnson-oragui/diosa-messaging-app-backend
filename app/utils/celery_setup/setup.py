b4268b1e3b115047a49ecb6539eb3c6222303582
johnson-oragui <johnson.oragui@gmail.com>
2025-06-03 15:41:39 +0100

    feat: added send_email_in_background for seding   emails using celery

--- app/utils/celery_setup/setup.py
+++ app/utils/celery_setup/setup.py
@@ -11,31 +11,29 @@ def make_celery(app=None, broker_url=None, result_backend=None):
     if app:
         name = app.__name__
     if not broker_url and not result_backend:
-        if settings.mode == "DEV":
+        if settings.mode == "TEST":
             broker_url = settings.celery_broker_url_test
             result_backend = settings.celery_result_backend_test
 
     # Set up Celery with custom broker and result backend
     celery = Celery(
         name,
-         broker=broker_url or settings.celery_broker_url,
-         backend=result_backend or settings.celery_result_backend,
+        broker=broker_url or settings.celery_broker_url,
+        backend=result_backend or settings.celery_result_backend,
     )
 
     # add celery configurations
-    celery.config_from_object('app.core.celery_config')
+    celery.config_from_object("app.core.celery_config")
 
     # Automatically discover tasks from the specified module
-    celery.autodiscover_tasks(
-        ['app.utils.celery_setup.tasks'],
-        related_name='tasks'
-    )
+    celery.autodiscover_tasks(["app.utils.celery_setup.tasks"], related_name="tasks")
 
     return celery
+
 
 # Create a Celery app instance for production
 app = make_celery()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     app.start()
