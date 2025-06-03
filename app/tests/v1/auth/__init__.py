81efc9a4e82aa560d28214f0ba9ea6012486470c
johnson-oragui <johnson.oragui@gmail.com>
2025-06-03 15:43:10 +0100

    feat: added e2e tests for reset password

--- app/tests/v1/auth/__init__.py
+++ app/tests/v1/auth/__init__.py
@@ -23,6 +23,12 @@ password_change_register_input = {
     "confirm_password": "Johnson1234#",
     "idempotency_key": "1234567z9012-1234-2w3e-4r5t-6y7u8i9z",
 }
+password_reset_register_input = {
+    "email": "johnson1@gtest.com",
+    "password": "Johnson1234#",
+    "confirm_password": "Johnson1234#",
+    "idempotency_key": "1234567z9012-1234-2w3e-4r5t-6y7u8i9z",
+}
 
 
 async def delete_user(session: AsyncSession, email: str) -> None:
