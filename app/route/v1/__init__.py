ee8c48077e0dba979896410746555a8811e95ff5
johnson-oragui <johnson.oragui@gmail.com>
2025-06-08 13:20:20 +0100

    feat: added media upload routes

--- app/route/v1/__init__.py
+++ app/route/v1/__init__.py
@@ -1,9 +1,15 @@
+"""
+Router Init
+"""
+
 from fastapi import APIRouter
 
 from app.route.v1.auth_route import auth_router
 from app.route.v1.direct_message_route import direct_message_router
+from app.route.v1.media_upload_route import media_upload_router
 
 api_version_one = APIRouter(prefix="/api/v1")
 
 api_version_one.include_router(auth_router)
 api_version_one.include_router(direct_message_router)
+api_version_one.include_router(media_upload_router)
