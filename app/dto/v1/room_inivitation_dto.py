f6fe2410f13de2b2579d274f4d70e953237a6dc1
johnson-oragui <johnson.oragui@gmail.com>
2025-06-17 11:54:40 +0100

    feat: added room invitation dto

---
+++ app/dto/v1/room_inivitation_dto.py
@@ -0,0 +1,54 @@
+"""
+Room invitation dto module
+"""
+
+from typing import Annotated
+from datetime import datetime
+
+from pydantic import BaseModel, Field, ConfigDict, StringConstraints
+
+
+class RoomInvitationBaseDto(BaseModel):
+    """
+    RoomInvitationBaseDto
+    """
+
+    id: str = Field(examples=["312423-4-3544354-5-41342165"])
+    invitee_id: str = Field(examples=["312423-4-3544354-5-41342165"])
+    room_id: str = Field(examples=["312423-4-3544354-5-41342165"])
+    invitation_status: str = Field(examples=["pending"])
+    expiration: datetime = Field(examples=[datetime.now()])
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+# +++++++++++++++++++++++++++++++++++++++ ROom invitation request +++++++++++++++++++++++++++===
+
+
+class RoomInvitationRequestDto(BaseModel):
+    """
+    RoomInvitationRequestDto
+    """
+
+    invitee_id: Annotated[
+        str, StringConstraints(min_length=20, max_length=40, strip_whitespace=True)
+    ] = Field(examples=["312423-4-3544354-5-41342165"])
+    room_id: Annotated[
+        str, StringConstraints(min_length=20, max_length=40, strip_whitespace=True)
+    ] = Field(examples=["312423-4-3544354-5-41342165"])
+
+
+class RoomInvitationResponseDto(BaseModel):
+    """
+    RoomInvitationResponseDto
+    """
+
+    status_code: int = Field(default=201, examples=[201])
+    message: str = Field(
+        default="Invitation sent successfully",
+        examples=["Invitation sent successfully"],
+    )
+    data: RoomInvitationBaseDto
+
+
+# +++++++++++++++++++++++++++++++++++++++ ROom invitation accept/reject +++++++++++++++++++++++++++===
