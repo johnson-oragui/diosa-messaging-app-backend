77f90b19441b603384f870dc359ecf13ad5db5e9
johnson-oragui <johnson.oragui@gmail.com>
2025-06-19 10:37:47 +0100

    feat: updated fetch query to include optional invitation id

--- app/repository/v1/room_invitation_repository.py
+++ app/repository/v1/room_invitation_repository.py
@@ -19,7 +19,15 @@ class RoomInvitationRepository:
         self, session: AsyncSession, room_id: str, inviter_id: str, invitee_id: str
     ) -> RoomInvitation:
         """
-        Creates a room invitation
+        Creates a room invitation.
+
+        Args:
+            session (AsyncSession): The database async session object.
+            room_id (str): The id of the Room for invitation.
+            inviter_id (str): The id of the user inviting another user.
+            invitee_id (str): The id of the user been invited by another user.
+        Returns:
+            RoomInvitation (object): The room-invitation instance.
         """
         expiration = datetime.now(timezone.utc) + timedelta(days=7)
         new_invitation = RoomInvitation(
@@ -36,21 +44,36 @@ class RoomInvitationRepository:
     async def fetch(
         self,
         session: AsyncSession,
-        room_id: str,
+        room_id: typing.Union[None, str],
         inviter_id: typing.Union[None, str],
         invitee_id: typing.Union[None, str],
         invitation_status: typing.Union[None, str],
+        invitation_id: typing.Union[None, str],
     ) -> typing.Union[None, RoomInvitation]:
         """
-        Fetches a room invitation
+        Fetches a room invitation.
+
+        Args:
+            session (AsyncSession): The database async session object.
+            room_id (str): The Optional id of the Room for invitation.
+            inviter_id (str): The Optional id of the user inviting another user.
+            invitee_id (str): The Optional id of the user been invited by another user.
+            invitation_status (str): The Optional status of the invitation.
+            invitation_id (str): The Optional id of the invitation.
+        Returns:
+            int: The number of affected rows
         """
-        query = sa.select(RoomInvitation).where(RoomInvitation.room_id == room_id)
+        query = sa.select(RoomInvitation)
+        if room_id:
+            query = query.where(RoomInvitation.room_id == room_id)
         if inviter_id:
             query = query.where(RoomInvitation.inviter_id == inviter_id)
         if invitee_id:
             query = query.where(RoomInvitation.invitee_id == invitee_id)
         if invitation_status:
             query = query.where(RoomInvitation.invitation_status == invitation_status)
+        if invitation_id:
+            query = query.where(RoomInvitation.id == invitation_id)
 
         return (await session.execute(query)).scalar_one_or_none()
 
@@ -58,13 +81,23 @@ class RoomInvitationRepository:
         self, session: AsyncSession, invitation_id: str, status: str
     ) -> int:
         """
-        Updates room invitation
+        Updates room invitation.
+
+        Args:
+            session (AsyncSession): The database async session object.
+            invitation_id (str): The id of the invitation.
+            status (str): The status of the invitation.
+        Returns:
+            int: The number of affected rows
         """
+        expiration = datetime.now(timezone.utc) + timedelta(days=7)
         query = (
             sa.update(RoomInvitation)
             .where(RoomInvitation.id == invitation_id)
             .values(invitation_status=status)
         )
+        if status == "pending":
+            query = query.values(expiration=expiration, invitation_status=status)
 
         result = await session.execute(query)
         await session.commit()
