515c4f99c9435eef37557aa12f2837ebd696092f
johnson-oragui <johnson.oragui@gmail.com>
2025-06-24 17:47:11 +0100

    feat: added get_redis_client for redis async generator resolve

--- app/database/redis_db.py
+++ app/database/redis_db.py
@@ -1,12 +1,17 @@
+"""
+Redis session module
+"""
+
 from contextlib import contextmanager, asynccontextmanager
 import redis
 import redis.asyncio as aio_redis
+from redis.asyncio import Redis
 from tenacity import retry, wait_fixed, stop_after_attempt
 
 from app.core.config import settings
+from app.utils.task_logger import create_logger
 
-
-REDIS_URL: str = settings.redis_url
+logger = create_logger(":: REDIS SESSION ::")
 
 
 @contextmanager
@@ -18,15 +23,15 @@ def get_redis_sync():
     Connection to redis.
     """
     conn = redis.from_url(
-        url=REDIS_URL,
+        url=settings.redis_url,
         max_connections=10,
         decode_responses=True,
     )
     try:
         yield conn
     except redis.ConnectionError as exc:
-        print(f"Redis connection error: {exc}")
-        raise
+        logger.error("Redis connection error: %s", str(exc))
+        raise exc
 
 
 @asynccontextmanager
@@ -38,14 +43,31 @@ async def get_redis_async():
     Asynchronous connection to Redis.
     """
     conn = aio_redis.from_url(
-        url=REDIS_URL,
+        url=settings.redis_url,
         max_connections=10,
         decode_responses=True,
     )
     try:
         yield conn
     except redis.ConnectionError as exc:
-        print(f"Redis connection error: {exc}")
-        raise
+        logger.error("Redis connection error: %s", str(exc))
+        raise exc
+    except RuntimeError as exc:
+        logger.error("Redis connection RuntimeError error: %s", str(exc))
+        raise exc
     finally:
-        await conn.aclose()
+        # if conn:
+        #     await conn.aclose()
+        pass
+
+
+async def get_redis_client() -> Redis:
+    """
+    Resolves the Redis client from the context manager.
+    """
+    try:
+        async with get_redis_async() as redis_:
+            return redis_
+    except RuntimeError as exc:
+        logger.error("Redis RuntimeError error: %s", str(exc))
+        raise exc
