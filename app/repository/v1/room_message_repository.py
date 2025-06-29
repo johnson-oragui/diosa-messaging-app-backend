71b8516ef99eecbcb9439f16686b9065fa950c48
johnson-oragui <johnson.oragui@gmail.com>
2025-06-29 10:38:22 +0100

    feat: added fetch_all of room_message_repository

--- app/repository/v1/room_message_repository.py
+++ app/repository/v1/room_message_repository.py
@@ -62,5 +62,61 @@ class RoomMessageRepository:
 
         return new_message
 
+    async def fetch_all(
+        self,
+        room_id: str,
+        offset: int,
+        order: str,
+        session: AsyncSession,
+        attributes: typing.List[typing.Optional[str]] = [],
+    ) -> typing.Tuple[typing.Sequence[typing.Optional[RoomMessage]], int]:
+        """
+        Retrieves all messages.
+
+        Args:
+            room_id(str): The id of the room
+            offset(int): The offset for pagination
+            order(str): The order by (e., asc, desc).
+            session (AsyncSession): The database async session object.
+            attributes (List[str]): Optional list of fields to select from the message
+        Returns:
+            List[Optional[RoomMessage]]: List containing optional room messages.
+        """
+        order_by = sa.desc if order == "desc" else sa.asc
+        if len(attributes) > 0:
+            selected_fields = [
+                getattr(self.model, attr)
+                for attr in attributes
+                if isinstance(attr, str) and hasattr(self.model, attr)
+            ]
+            if not selected_fields:
+                return [], 0
+            query = sa.select(*selected_fields).where(
+                self.model.room_id == room_id, self.model.is_deleted.is_(False)
+            )
+
+            count_stmt = sa.select(sa.func.count()).select_from(query.subquery())
+            count_result = await session.execute(count_stmt)
+            total_count = count_result.scalar_one() or 0
+
+            query = query.offset(offset).order_by(order_by(self.model.created_at))
+
+            result = (await session.execute(query)).scalars().all()
+
+            return (result, total_count)
+        query = sa.select(self.model).where(
+            self.model.room_id == room_id, self.model.is_deleted.is_(False)
+        )
+
+        count_stmt = sa.select(sa.func.count()).select_from(query.subquery())
+        count_result = await session.execute(count_stmt)
+        total_count = count_result.scalar_one() or 0
+
+        query = query.offset(offset).order_by(order_by(self.model.created_at))
+
+        result = (await session.execute(query)).scalars().all()
+
+        return (result, total_count)
+
 
 room_message_repository = RoomMessageRepository()
